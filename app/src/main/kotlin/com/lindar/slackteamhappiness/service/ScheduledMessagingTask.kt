674a6f4b7741949411c4217e1dee8887f2a83ba6
Erica Tanti <ericathedev@gmail.com>
2024-02-21 16:58:20 +0100

    Remove commented out zone

--- app/src/main/kotlin/com/lindar/slackteamhappiness/service/ScheduledMessagingTask.kt
+++ app/src/main/kotlin/com/lindar/slackteamhappiness/service/ScheduledMessagingTask.kt
@@ -14,8 +14,7 @@ class ScheduledMessagingTask(private val slackMessagingService: SlackMessagingSe
                              @Value("\${slack.teams.eng-team}") private val SLACK_TEAM_GROUP_ID: String?,
                              @Value("\${slack.teams.test-user}") private val SLACK_TEST_USER_ID: String?) {
 
-    @Scheduled(cron = "#{'\${schedule.cron}'}")
-//    @Scheduled(cron = "0 45 14 * * FRI", zone = "CET")
+    @Scheduled(cron = "#{'\${schedule.cron}'}", zone = "CET")
     fun sendWeeklyHappinessSurvey() {
         if (!SLACK_TEST_USER_ID.isNullOrEmpty()) {
             println("TESTING MODE: Sending to a single user ${SLACK_TEST_USER_ID}...")
